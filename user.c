24258d1a478c0bb6da75fd4876d44ceabead67f5
SAEZEUN <nathanlong86000@gmail.com>
2025-10-16 16:14:00 +0200

    add User.c
    movements basics

---
+++ user.c
@@ -0,0 +1,26 @@
+
+
+void Interaction() {
+    char input;
+    scanf("%c", &input);
+
+    switch (input) {
+
+        case 'z':
+        //standby
+        break;
+
+        case 'q':
+        //standby
+        break;
+
+        case 's':
+        //standby
+        break;
+
+        case 'd':
+        //standby
+        break;
+        
+    }
+}

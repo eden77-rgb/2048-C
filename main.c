ca7e562b50ba1ff5c14ff8d2c2c837d75e993c3f
nathan86440 <nathanlong86000@gmail.com>
2025-10-16 23:15:31 +0200

    voiture sans carosserie

--- main.c
+++ main.c
@@ -16,10 +16,11 @@ int main()
 
     Case **tab = creer_grille(size);
     generer_case(tab, case_vide(tab, size), count_case(tab, size));
-    afficherGrille(tab, size);
-    Interaction(tab, size);
     
-
+    while (case_vide(tab,size) != 0) {
+        afficherGrille(tab, size);
+        Interaction(tab, size);
+    }
     free(tab);
     return 0;
 }

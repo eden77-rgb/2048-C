17e0f54157317a807c6f9e9ca9f2822977e9b30e
Eden <eden.vandewatyne@estiam.com>
2025-10-16 14:05:32 +0200

    Création du type Case et ajout de la fusion des case

---
+++ case.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct
+{
+    int valeur;
+    int x;
+    int y;
+} Case;
+
+
+bool check_case(Case *c1, Case *c2) {
+    if (c1->valeur == c2->valeur)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+
+void add_valeur(Case *c1, Case *c2) 
+{
+    if (check_case(c1, c2))
+    {
+        c1->valeur += c2->valeur;
+        c2->valeur = 0;
+    }
+}
+
+
+int main()
+{
+    return 0;
+}

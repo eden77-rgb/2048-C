6fd16305de0fab1f49d43a0fb511a71af627d69a
Eden <eden.vandewatyne@estiam.com>
2025-10-17 11:58:25 +0200

    Ajout d'un condition d'arret

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ gcc -o output/main main.c
 
 int main()
 {
-    int size = 5;
+    int size = 3;
 
     Case **tab = creer_grille(size);
     generer_case(tab, case_vide(tab, size), count_case(tab, size));
@@ -30,6 +30,13 @@ int main()
         Interaction(tab, size);
         generer_case(tab, case_vide(tab, size), count_case(tab, size));
         afficherGrille(tab, size);
+
+        if (count_case(tab, size) <= 0)
+        {
+            running = false;
+            printf("Fin du jeu, merci d'avoir\n");
+        }
+        
     }
 
     free(tab);
